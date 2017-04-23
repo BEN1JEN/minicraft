7cc616813fa80ac7904f5dbadf5cc628cadc9d48
BEN1JEN <benpheard@gmail.com>
2017-04-23 17:16:16 -0600

    livestreem update 4
    more progress on world interaction

--- func/worldInteraction.lua
+++ func/worldInteraction.lua
@@ -2,24 +2,32 @@ worldInteraction = {}
 
 function worldInteraction.update(hotBarSelect, world, inventory, blocks, items, playerX, playerY)
 
+  local place = false
+
   if love.mouse.isDown(1) then
 
-    mouseX, mouseY = love.mouse.getPosition()
+    if not(mDownLast) then
 
-    clickX, clickY =
-    math.floor( (playerX + mouseX - 512)/10 + 0.5 ) * 10,
-    math.floor( (playerX + mouseY - 360)/10 + 0.5 ) * 10
+      mouseX, mouseY = love.mouse.getPosition()
 
-    for x = 1, 10 do
-      for y = 1, 5 do
-        if inventory[x][y] == 0 then inventory[x][y] = world[clickX][clickY]["ID"] print(world[clickX][clickY]["ID"]) break end
+      clickX, clickY =
+      math.floor( (playerX + (mouseX - 512)/10) + 0.5 ),
+      math.floor( (playerY + (720 - mouseY - 360)/10) + 0.5 )
+
+      for x = 1, 10 do
+        for y = 1, 5 do
+          if inventory[x][y] == 0 and not(placed) then inventory[x][y] = world[clickX][clickY]["ID"] placed = true end
+        end
       end
+      placed = false
+
+      world[clickX][clickY]["name"] = "air"
+      world[clickX][clickY]["id"] = 0
+
     end
-
-    world[clickX][clickY]["name"] = "air"
-    world[clickX][clickY]["id"] = 0
-    print(clickX, clickY)
-
+    mDownLast = true
+  else
+    mDownLast = false
   end
 
   return world, inventory
